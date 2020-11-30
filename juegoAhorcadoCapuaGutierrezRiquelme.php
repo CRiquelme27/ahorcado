8b8cfc4742b137c5c5a035c7961ee8284b410ede
CRISTIAN RIQUELME <74301855+CRiquelme27@users.noreply.github.com>
2020-11-30 15:52:51 -0300

    Update juegoAhorcadoCapuaGutierrezRiquelme.php

--- juegoAhorcadoCapuaGutierrezRiquelme.php
+++ juegoAhorcadoCapuaGutierrezRiquelme.php
@@ -356,7 +356,8 @@ function jugar($coleccionPalabras, $indicePalabra, $cantIntentos){
         echo "             /|\  |\n";
         echo "             / \  |\n";
         echo "                  |\n";
-        echo "=================================\n" ;
+        echo "=================================\n" ; 
+        echo "La palabra era: " . $pal ."\n";
     }
     return $puntaje;
 }
